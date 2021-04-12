2fbc5aa2a05247a4bf8beb7b43639ed5f45af272
Thomas Brannan <th.c.brannan@gmail.com>
2021-04-12 14:59:30 -0700

    Decided that the Recursive approach would be inferior to the iterative. Wrote an iterative method to check for tripple character sequence weaknesses in the password. Perhaps this should be done with regex.

--- StrongPasswordChecker-Iterative.js
+++ StrongPasswordChecker-Iterative.js
@@ -26,17 +26,17 @@ const replace = function(options=null) {
 };
 
 const isShort = function() {
-  return (this.length < 6);
+  return (this.text.length < 6);
 };
 
 const isLong = function() {
-  return (this.length > 20);
+  return (this.text.length > 20);
 };
 
 const UPPER_ALPHABET = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y ,Z];
 const hasUpper = function() {
-  for (let i = 0; i < this.length; i++) {
-    if (UPPER_ALPHABET.includes(this[i])) {
+  for (let i = 0; i < this.text.length; i++) {
+    if (UPPER_ALPHABET.includes(this.text[i])) {
       return true;
     }
   }
@@ -45,8 +45,8 @@ const hasUpper = function() {
 
 const LOWER_ALPHABET = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
 const hasLower = function() {
-  for (let i = 0; i < this.length; i++) {
-    if (LOWER_ALPHABET.includes(this[i])) {
+  for (let i = 0; i < this.text.length; i++) {
+    if (LOWER_ALPHABET.includes(this.text[i])) {
       return true;
     }
   }
@@ -54,33 +54,51 @@ const hasLower = function() {
 };
 
 const hasDigit = function() {
-  for (let i = 0; i < this.length; i++) {
-    if (Number(this[i])) {
+  for (let i = 0; i < this.text.length; i++) {
+    if (Number(this.text[i])) {
       return true;
     }
   }
   return false;
 };
 
+const hasTripples = function() {
+  // keep track of a list of indicies
+  // for each char in text:
+  //  if the next two chars are identical:
+  //    add the current index to the list
+  //    skip forward
+  let indicies = [];
+  let i = 0;
+  while (i < this.text.length) {
+    if (this.text[i] === this.text[i+1] && this.text[i+1] === this.text[i+2]) {
+      indicies.push(i);
+      i += 3;
+    } else {
+      i += 1;
+    }
+  }
+  return indicies;
+}
+
 /**
  * @param {string} password
  * @return {number}
  */
 const strongPasswordChecker = function(password) {
-  
-  password.prototype.insert = insert;
-  password.prototype.remove = remove;
-  password.prototype.replace = replace;
-  password.prototype.isShort = isShort;
-  password.prototype.isLong = isLong;
-  password.prototype.hasUpper = hasUpper;
-  password.prototype.hasLower = hasLower;
-  password.prototype.hasDigit = hasDigit;
-
-  let max = 0;
-  var iter = function(password, steps = 0) {
-
+  password = {
+    text: password,
+    insert,
+    remove,
+    replace
+    isShort,
+    isLong,
+    hasUpper,
+    hasLower,
+    hasDigit,
+    hasTripples
   };
-  iter(password);
-  return max;
+
+
+
 }
