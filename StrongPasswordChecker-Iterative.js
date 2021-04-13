a8f7d8699b7a5c9d61901ec5e2be5a538c6b5026
Thomas Brannan <th.c.brannan@gmail.com>
2021-04-13 00:05:44 -0700

    Fixed some simple syntax issues Implemented a version of replace()

--- StrongPasswordChecker-Iterative.js
+++ StrongPasswordChecker-Iterative.js
@@ -2,38 +2,58 @@
 
 /**
  * @param {Object} options
+ * @param {Number} target
  * @param {String} options.include, insert a char of this type
  */
 const insert = function(options=null) {
+  this.steps += 1;
 
 };
 
 /**
  * @param {Object} options
+ * @param {Number} target
  * @param {Array[String]} options.exclude, do NOT remove a char of any of these types
  */
 const remove = function(options=null) {
+  this.steps += 1;
 
 };
 
 /**
  * @param {Object} options
+ * @param {Number} target
  * @param {String} options.include, insert a char of this type
  * @param {Array[String]} options.exclude, do NOT remove a char of any of these types
  */
 const replace = function(options=null) {
-
+  this.steps += 1;
+  let char = '0';
+  if (options.include) {
+    char = (options.include === 'upper') ? 'A' : (options.include === 'lower') ? 'a' : char;
+  }
+  let target = options.target || 0;
+  this.text = this.text.slice(0, target) + char + this.text.slice(target+1);
 };
 
+/**
+ * @returns {Boolean}
+ */
 const isShort = function() {
   return (this.text.length < 6);
 };
 
+/**
+ * @returns {Boolean}
+ */
 const isLong = function() {
   return (this.text.length > 20);
 };
 
-const UPPER_ALPHABET = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y ,Z];
+const UPPER_ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+/**
+ * @returns {Boolean}
+ */
 const hasUpper = function() {
   for (let i = 0; i < this.text.length; i++) {
     if (UPPER_ALPHABET.includes(this.text[i])) {
@@ -43,7 +63,10 @@ const hasUpper = function() {
   return false;
 };
 
-const LOWER_ALPHABET = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+const LOWER_ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+/**
+ * @returns {Boolean}
+ */
 const hasLower = function() {
   for (let i = 0; i < this.text.length; i++) {
     if (LOWER_ALPHABET.includes(this.text[i])) {
@@ -53,6 +76,9 @@ const hasLower = function() {
   return false;
 };
 
+/**
+ * @returns {Boolean}
+ */
 const hasDigit = function() {
   for (let i = 0; i < this.text.length; i++) {
     if (Number(this.text[i])) {
@@ -62,6 +88,9 @@ const hasDigit = function() {
   return false;
 };
 
+/**
+ * @returns {Array[Numbers]}, indicies of tripples, [] if no tripples
+ */
 const hasTripples = function() {
   // keep track of a list of indicies
   // for each char in text:
@@ -86,11 +115,13 @@ const hasTripples = function() {
  * @return {number}
  */
 const strongPasswordChecker = function(password) {
+
   password = {
     text: password,
+    steps: 0,
     insert,
     remove,
-    replace
+    replace,
     isShort,
     isLong,
     hasUpper,
@@ -99,6 +130,49 @@ const strongPasswordChecker = function(password) {
     hasTripples
   };
 
+  let data = {
+    short: password.isShort(),
+    long: password.isLong(),
+    upper: password.hasUpper(),
+    lower: password.hasLower(),
+    digit: password.hasDigit(),
+    tripples: password.hasTripples(),
+  };
 
+  let target, action, options;
+
+  while(data.tripples.length) {
+
+    options = {
+      target: data.tripples[data.tripples.length - 1] + 2,
+      include: (data.long) ? null : (!data.upper) ? 'upper' : (!data.lower) ? 'lower' : 'digit'
+    };
+
+    if (data.short) {
+      password.insert(options);
+    } else if (data.long) {
+      password.remove(options);
+    } else {
+      password.replace(options);
+    }
+
+    if (options.include) {
+      if (options.include === 'upper') {
+        data.upper = true;
+      } else if (options.include === 'lower') {
+        data.lower = true;
+      } else if (options.include === 'digit') {
+        data.digit = true;
+      }
+    }
+
+    data.tripples.pop();
+
+  };
+
+  // handle any post-tripple situations
+
+  return password.steps;
 
 }
+
